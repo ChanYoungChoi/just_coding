4e78edc6382a42e3694a9b5b98b91a72d11850d2
Choi Chan Young <chan91@mailplug.co.kr>
2021-07-24 17:55:07 +0900

    log4j 공부

---
+++ src/main/java/com/example/chan91/Chan91Application.java
@@ -0,0 +1,13 @@
+package com.example.chan91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chan91Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chan91Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chan91/Chan91ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chan91;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chan91ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
